47fc75d23b06a937fb0eec5f814612f62cb53138
Emmanuelaffairs <129261686+Emmanuelaffairs@users.noreply.github.com>
2023-08-26 04:12:07 +0100

    Add files via upload

---
+++ forms/contact.php
@@ -0,0 +1,44 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'abikoyeboluwatife@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $register = new PHP_Email_Form;
+  $register->ajax = true;
+  
+  $register->to = $receiving_email_address;
+  $register->from_name = $_POST['name'];
+  $register->from_email = $_POST['email'];
+  $register->from_phone = $_POST['phone'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  $register->add_message( $_POST['name'], 'From');
+  $register->add_message( $_POST['email'], 'Email');
+  $register->add_message( $_POST['phone'], 'Mobile no');
+  $register->add_message( $_POST['destination'], 'Preferred Study Destination');
+  $register->add_message( $_POST['intake'], 'Preferred Intake');
+  $register->add_message( $_POST['qualification'], 'Qualificcation', 4);
+
+  echo $contact->send();
+?>
