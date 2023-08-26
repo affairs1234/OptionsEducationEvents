ba9ab4410455e7e69d4226acbc25c4e10ca8ec28
Emmanuelaffairs <129261686+Emmanuelaffairs@users.noreply.github.com>
2023-08-26 11:49:53 +0100

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,32 +1,20 @@
 <?php
 
   $receiving_email_address = 'abikoyeboluwatife@gmail.com';
+//send form data to receiving email address
+  if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $destination = $_POST['destination'];
+    $intake = $_POST['intake'];
+    $qualification = $_POST['qualification'];
 
-  if( file_exists($php_email_form = '../files/functionalities/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    $mailTo = "abikoyeboluwatife@gmail.com";
+    $headers = "From: ".$email;
+    $txt = "You have received an email from ".$name;
+
+    mail($mailTo, $email, $phone, $destination, $intake, $qualification, $txt, $headers);
+    header("Location: index.php?mailsend");
   }
-
-  $register = new PHP_Email_Form;
-  $register->ajax = true;
-  
-  $register->to = $receiving_email_address;
-  $register->from_name = $_POST['name'];
-  $register->from_email = $_POST['email'];
-  $register->from_phone = $_POST['phone'];
-  $register->from_destination = $_POST['destination'];
-  $register->from_intake = $_POST['intake'];
-  $register->from_qualification = $_POST['qualification'];
-  $register->subject = 'New Registration';
-
-
-  $register->add_message( $_POST['name'], 'From');
-  $register->add_message( $_POST['email'], 'Email');
-  $register->add_message( $_POST['phone'], 'Mobile no');
-  $register->add_message( $_POST['destination'], 'Preferred Study Destination');
-  $register->add_message( $_POST['intake'], 'Preferred Intake');
-  $register->add_message( $_POST['qualification'], 'Qualificcation', 4);
-
-  echo $contact->send();
 ?>
